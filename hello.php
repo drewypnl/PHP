1060d5c5c367d7d352c1d7013bcc8db0dcfa4a39
steve <drewysoft@hotmail.com>
2017-04-17 08:52:24 +0100

    Added hello.php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World Script</title>
+</head>
+<body>
+<?php
+echo "<p>Hello World!</p>";
+?>
+<p>This is HTML only.</p>
+<?php
+echo "<p>Hello World again!</p>";
+?>
+<p>This is a second HTML section.</p>
+</body>
