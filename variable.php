5ae53e4b163c2294ebbdb0eb54d6fe68ca5b4a2b
steve <drewysoft@hotmail.com>
2017-04-17 09:39:21 +0100

    Added hello world to variable.php

--- variable.php
+++ variable.php
@@ -1,7 +1,16 @@
 <?php
-$Reno = 360000;
-$Pasadena = 138000;
-$cityname = "Reno";
-echo "The size of $cityname is ${$cityname} <br />";
-$cityname = "Pasadena";
-echo "The size of $cityname is ${$cityname}";
+    $Reno = 360000;
+    $Pasadena = 138000;
+    $cityname = "Reno";
+    echo "The size of $cityname is ${$cityname} <br />";
+    $cityname = "Pasadena";
+    echo "The size of $cityname is ${$cityname}";
+?>
+<p>Hello World</p>
+<?php
+    $age = 15;
+    $name = "Harry";
+?>
+<p>Hello World again!!</p>
+<?php
+    echo $name;
