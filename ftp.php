2deb0208951b6f79429eea53faa987acd3e83bb8
steve <drewysoft@hotmail.com>
2017-04-25 20:47:02 +0100

    Added ftpstuff.inc

--- ftp.php
+++ ftp.php
@@ -6,16 +6,18 @@ $dir_name = "/";
 $connect = ftp_connect($servername) or die("Can't connect to FTP server");
 $login_result = ftp_login($connect, $userID, $passwd) or die("Can't log in");
 $filesArr = ftp_nlist($connect, $dir_name);
-foreach($filesArr as $value) {
-    if( preg_match("#/. txt$#", $value))
+foreach($filesArr as $value)
+{
+    if( preg_match("#/.txt$#", $value))
     {
-    if(!file_exists($value))
-    {
-        ftp_get($connect, $value, $dir_name. $value, FTP_ASCII);
-    }
-    } else {
+        if(!file_exists($value))
+        {
+            ftp_get($connect, $value, $dir_name. $value, FTP_ASCII);
+        }
+    } else
+        {
         echo "File $value already exists!\n";
-    }
+        }
 }
 ftp_close($connect);
 
